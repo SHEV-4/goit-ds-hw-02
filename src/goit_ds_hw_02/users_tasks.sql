ea01383e660776d42760034efacce3000a5e9a6c
Yurii <59282993+SHEV-4@users.noreply.github.com>
2025-06-29 11:47:53 +0300

    Update users_tasks.sql

--- src/goit_ds_hw_02/users_tasks.sql
+++ src/goit_ds_hw_02/users_tasks.sql
@@ -20,7 +20,7 @@ CREATE TABLE tasks(
     user_id INTEGER,
     FOREIGN KEY (status_id) REFERENCES status (id)
         ON DELETE CASCADE
-        ON UPDATE CASCADE
+        ON UPDATE CASCADE,
 
     FOREIGN KEY (user_id) REFERENCES users (id)
         ON DELETE CASCADE
